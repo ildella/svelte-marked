7ac3ccb23f58d438fb890913e51331db048ad000
Daniele Dellafiore <66707+ildella@users.noreply.github.com>
2025-02-10 10:59:49 +0100

    cleanup commented code

--- src/SvelteMarkdown.svelte
+++ src/SvelteMarkdown.svelte
@@ -1,6 +1,4 @@
 <script>
-  import { run } from 'svelte/legacy'
-
   import { setContext, createEventDispatcher, onMount } from 'svelte'
   import { marked } from 'marked'
   import Slugger from 'github-slugger'
@@ -16,42 +14,27 @@
     isInline = false
   } = $props()
 
-  // const dispatch = createEventDispatcher()
-
   let tokens = $state()
-  // let mounted = $state()
 
   let preprocessed = $derived(Array.isArray(source))
   let slugger = $derived(source ? new Slugger : undefined)
   let combinedOptions = $derived({ ...defaultOptions, ...options })
   let combinedRenderers = $derived({ ...defaultRenderers, ...renderers })
 
-  // function dispatch(eventName, detail) {
-  //   const event = new CustomEvent(eventName, { detail });
-  //   dispatchEvent(event);
-  // }
-  
   $effect(() => {
     if (preprocessed) {
       tokens = source
     } else {
       const lexer = new marked.Lexer(combinedOptions)
       tokens = isInline ? lexer.inlineTokens(source) : lexer.lex(source)
-      // dispatch('parsed', { tokens })
     }
   })
-  // run(() => {
-  //   mounted && !preprocessed && dispatch('parsed', { tokens })
-  // })
 
   setContext(key, {
     slug: (val) => slugger ? slugger.slug(val) : '',
     getOptions: () => combinedOptions
   })
 
-  // onMount(() => {
-  //   mounted = true
-  // })
 </script>
 
 <Parser {tokens} renderers={combinedRenderers} />
